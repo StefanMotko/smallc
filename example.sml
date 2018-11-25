7231bc33fad1e45c99d81d9fbc2d32ac22d8da82
Stefan Motko <stefanmotko@gmail.com>
2018-11-25 20:27:13 +0100

    Initial commit

---
+++ example.sml
@@ -0,0 +1,1 @@
+BEGIN write ( a ) ; END
---
+++ smallc.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+if (process.argv.length != 3) {
+
+}
+
+fs.readFile(process.argv[2], (err, input) => {
+
+    
+
+});
