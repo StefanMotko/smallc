6ce300398eb41d83888ef668affe6fbe3f964ed8
Stefan Motko <stefanmotko@gmail.com>
2018-11-26 17:27:27 +0100

    Lexer WIP

--- smallc.js
+++ smallc.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const tokens = require('./tokens.json');
 
 if (process.argv.length != 3) {
-
+    console.log(`   smallc - SMALL language compiler
+    Usage:
+        smallc <file>
+        
+    <file>  file to compile`);
+    // exit succesfully with no args, error on multiple args
+    process.exit(process.argv.length == 2 ? 0 : 1);
 }
 
 fs.readFile(process.argv[2], (err, input) => {
 
-    
+    if (err) {
+        throw err;
+    }
+
+    tokens.map(token => new RegExp(token.pattern));
+
 
 });
